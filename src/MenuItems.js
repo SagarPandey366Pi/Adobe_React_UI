7643110e5ded71af42c2465b66e85cc846b93ddc
sagar <sagar.pandey15@gmail.com>
2021-02-03 14:13:24 +0530

    First commit

---
+++ src/MenuItems.js
@@ -0,0 +1,17 @@
+export const MenuItems = [
+    {
+        title: 'Creative Cloud',
+        url: '#',
+        cName: 'nav-links'
+    },
+    {
+        title: 'Experience Cloud',
+        url: '#',
+        cName: 'nav-links'
+    },
+    {
+        title: 'Document Cloud',
+        url: '#',
+        cName: 'nav-links'
+    }
+]
